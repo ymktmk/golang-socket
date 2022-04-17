fa40e232af4750fdc2bc48737b039c737c9a7e1e
ymktmk <73768462+ymktmk@users.noreply.github.com>
2022-04-18 00:59:01 +0900

    [add] client.go

--- client.go
+++ client.go
@@ -6,26 +6,26 @@ import (
 	"os"
 )
 
+// go run client.go 127.0.0.1:10000
+
 func main() {
-	// ①実行の際に指定したhost:portでbind
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "Usage: %s host:port ", os.Args[0])
 		os.Exit(1)
 	}
 	service := os.Args[1]
-	fmt.Println(service)
-	// ②ソケットの作成とIP:portに紐付け
+	// ソケットの作成 & 接続相手のIPアドレスとポート番号を設定
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
 	if err != nil {
 	}
-	// ③サーバ側に接続
+	// 接続の要求を行う
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	// ④ソケットにデータの書き込み
+	// ソケットにデータの書き込み
 	_, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
 	res := make([]byte, 1024)
-	// ⑤ソケットからデータの読み込み
+	// ソケットからデータの読み込み
 	len, err := conn.Read(res)
 	fmt.Println("response:", string(res[:len]))
-	// ⑥接続の切断
+	// ソケットを閉じる
 	conn.Close()
 }
